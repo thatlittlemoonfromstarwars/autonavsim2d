7b3fae621fa0686d372213a5e87d06ab8e3bacef
Andor Siegers <andor.siegers@gmail.com>
2024-07-05 12:05:32 -0600

    Fixed rectangle drawing to make corners generic

--- src/autonavsim2d/utils/map_save_and_load.py
+++ src/autonavsim2d/utils/map_save_and_load.py
@@ -8,39 +8,45 @@ def load_map(grid, file_path):
         "obstacles": [
             {
                 "type": "rectangle",
-                "top_left": [1, 1],
-                "bottom_right": [3, 3]
+                "corner1": [50, 10],
+                "corner2": [55, 50]
             },
             {
                 "type": "circle",
-                "center": [5, 5],
-                "radius": 2
+                "center": [50, 100],
+                "radius": 20
             },
             {
                 "type": "triangle",
                 "vertices": [
-                    [7, 7],
-                    [9, 7],
-                    [8, 9]
+                    [100, 100],
+                    [125, 110],
+                    [100, 150]
                 ]
             }
         ]
     }
+
     '''
     if file_path is None:
         return grid
     
-    def draw_rectangle(top_left, bottom_right):
-        for x in range(top_left[0], bottom_right[0] + 1):
-            for y in range(top_left[1], bottom_right[1] + 1):
-                grid[x][y][1] = BLACK
+    def draw_rectangle(corner1, corner2):
+        min_x = min(corner1[0], corner2[0])
+        max_x = max(corner1[0], corner2[0])
+        min_y = min(corner1[1], corner2[1])
+        max_y = max(corner1[1], corner2[1])
+
+        for x in range(min_x, max_x + 1):
+            for y in range(min_y, max_y + 1):
+                grid[y][x][1] = BLACK
 
     def draw_circle(center, radius):
         cx, cy = center
         for x in range(len(grid)):
             for y in range(len(grid[0])):
                 if (x - cx) ** 2 + (y - cy) ** 2 <= radius ** 2:
-                    grid[x][y][1] = BLACK
+                    grid[y][x][1] = BLACK
 
     def draw_triangle(vertices):
         def sign(p1, p2, p3):
@@ -56,14 +62,14 @@ def load_map(grid, file_path):
                 has_neg = (d1 < 0) or (d2 < 0) or (d3 < 0)
                 has_pos = (d1 > 0) or (d2 > 0) or (d3 > 0)
                 if not (has_neg and has_pos):
-                    grid[x][y][1] = BLACK
+                    grid[y][x][1] = BLACK
 
     with open(file_path, 'r') as file:
         data = json.load(file)
 
     for obstacle in data['obstacles']:
         if obstacle['type'] == 'rectangle':
-            draw_rectangle(obstacle['top_left'], obstacle['bottom_right'])
+            draw_rectangle(obstacle['corner1'], obstacle['corner2'])
         elif obstacle['type'] == 'circle':
             draw_circle(obstacle['center'], obstacle['radius'])
         elif obstacle['type'] == 'triangle':
